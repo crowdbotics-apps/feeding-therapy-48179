eabdca4719dfb65aec5c0dcacd354b3f65069e63
Crowdbotics <team@crowdbotics.com>
2024-05-30 14:58:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "feeding-therapy-48179",
+  url: "https://feeding-therapy-48179.botics.co",
+  api: "https://feeding-therapy-48179.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
